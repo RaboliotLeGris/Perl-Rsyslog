bd854225a9c1ea6e708187c77210c0fff9189a00
Raboliot38 <jordan@lovebox.love>
2018-01-25 13:49:45 +0100

    server logs are know writen in a special file

--- server/server.pl
+++ server/server.pl
@@ -26,6 +26,9 @@ my $params = Config::IniFiles->new( -file => "./config.ini");
 my $CIDR = NetAddr::IP->new($params->val("Server", "authorized"));
 # Creating a queue to send the logs and be written some days
 my $q = Thread::Queue->new();
+# To write into a file the server logs
+my $pathToLogServer = $params->val("Server", "logDir") . $params->val("Server", "logServerName");
+open(my $l, ">>",  $pathToLogServer) or die "Could not open file '$pathToLogServer' $!";
 
 # Creating the listening socket :
 my $socket = new IO::Socket::INET (
@@ -39,27 +42,32 @@ my $socket = new IO::Socket::INET (
 die "Unable to create the socket.\n     |-> Reason : $!\n\n" unless $socket;
 
 #############################################################################################################
+sub display {
+    my $payload = getTimestamp(). " : " .shift;
+    print $l $payload;
+    print $payload if $debug;
+}
+
 sub getTimestamp {
     my $timestamp = $params->val("Server", "timestampFormat");
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time); #Thanks : https://stackoverflow.com/questions/12644322/how-to-write-the-current-timestamp-in-a-perl-file
     # Not the cleanest thing on the Earth but it's "adaptable"
     my $customYear = $year+1900; my $customMonth = $mon + 1;
     $timestamp =~ s/YYYY/$customYear/ig;$timestamp =~ s/MM/$customMonth/ig;$timestamp =~ s/DD/$mday/ig;$timestamp =~ s/HH/$hour/ig;$timestamp =~ s/MM/$min/ig;$timestamp =~ s/SS/$sec/ig;
-    print $timestamp."\n" if $debug;
     return $timestamp; # YYYY-MM-DD HH:MM:SS  aka  2018-1-25 01:59:30
 }
 
 # This function dequeue the logs to write (So the server is not slow down with many I/Os)
 sub logsWriterWorker {
     my $path = $params->val("Server", "logDir") . $params->val("Server", "logName");
-    print $path."\n" if $debug;
+    display($path."\n");
     open(my $f, ">>",  $path) or die "Could not open file '$path' $!";
     # The "funny" thing with thread is that you have to flush them ...
     $f->autoflush;
     # Emptying the queue and writing with log the needed information the logs
     while (my $query = $q->dequeue) 
     {
-        print "writing into messages.log"."\n" if $debug;
+        display("writing into messages.log"."\n");
         print $f getTimestamp() . " : " . "$query\n";
     }
 }
@@ -67,7 +75,7 @@ sub logsWriterWorker {
 # To filter the connexions
 sub isAuthorized {
     my $clientIP = NetAddr::IP->new(shift);
-    print("Checking if $clientIP is authorized\n") if $debug;
+    display("Checking if $clientIP is authorized\n");
     return $clientIP->within($CIDR);
 }
 
@@ -80,28 +88,29 @@ sub handler {
     while (my $data = <$socket>) {
         chomp $data;
         # Add the received data to the queue
-        print "queuying "."\n" if $debug;
+        display("queuying "."\n");
         $q->enqueue($socket->peerhost() . " : " . $data);
         print $output "ack";
     }
     $socket->close();
-    print "Closing connection"."\n" if $debug;
+    display("Closing connection"."\n");
 }
 
 #############################################################################################################
 # Creating the working the write the logs on the logs file
 threads->create(\&logsWriterWorker);
-print "Server waiting for client connection on port " . $params->val("Server", "port") ."\n" if $debug;
+display("Server waiting for client connection on port " . $params->val("Server", "port") ."\n");
 while (1) {
     # Waiting for new connections
     my $client = $socket->accept();
-    print "new client from " . $client->peerhost() . "\n" if $debug;
+    display("new client from " . $client->peerhost() . "\n");
     if (isAuthorized( $client->peerhost() )) {
-        print("Creating thread\n") if $debug;
+        display("Creating thread\n");;
         # Creating a dedicated thread to process his logs
         threads->create(\&handler, $client);
     } else {
-        print "Unauthorized connection from " . $client->peerhost() . "\n" if $debug;
+        display("Unauthorized connection from " . $client->peerhost() . "\n");
     }
 }
 $socket->close();
+$l->close();
